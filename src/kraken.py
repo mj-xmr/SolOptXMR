dbccf281893613248657ce15ebe415a9556c52c6
endorxmr <28117225+endorxmr@users.noreply.github.com>
2022-04-15 23:36:41 +0200

    Implement fetching and saving historical ohlc data from kraken

--- src/kraken.py
+++ src/kraken.py
@@ -1,4 +1,7 @@
+from pykrakenapi import KrakenAPI
+from datetime import datetime
 from enum import Enum, auto
+import krakenex
 import requests
 
 
@@ -12,6 +15,8 @@ class fiat(Enum):
 
 
 class kraken:
+    api = krakenex.API()
+    k = KrakenAPI(api)
     pairs = {
         fiat.EUR: {
             coin.XMR: "XXMRZEUR",
@@ -39,9 +44,37 @@ class kraken:
             prices[fi] = float(coin_ticker["result"][pair]["c"][0])
         return prices
 
+    @staticmethod
+    def get_ohlc(co:coin, fi:fiat, interval=15, since=None, ascending=True):
+        pair = kraken.pairs[fi][co]
+        ohlc, last = kraken.k.get_ohlc_data(pair, interval=interval, since=since, ascending=ascending)
+        return ohlc, last
+
 
 if __name__ == "__main__":
-    a = kraken.get_price(coin.XMR, fiat.EUR)
-    b = kraken.get_prices(coin.XMR)
+    import pandas as pd
+    fi = fiat.EUR
+    co = coin.XMR
+    a = kraken.get_price(co, fi)
+    b = kraken.get_prices(co)
+    ohlc, last = kraken.get_ohlc(co, fi, interval=15, since=datetime.timestamp(datetime(2013, 1, 1, 0, 0, 0)), ascending=True)
+    ohlc.set_index("time")
     print(a)
     print(b)
+    # print(ohlc)
+    print(last)
+    pair = kraken.pairs[fi][co]
+    path = f"tmp/{pair}.pkl"
+    try: # If we have previous saved data, merge with the new data
+        # TODO: choose which values to keep for equal timestamps covering different time intervals
+        # Maybe keep them all, but in "parallel" dataframes? One for each time resolution.
+        # And when fetching data, look for the closest timestamp with the smallest resolution for best accuracy.
+        ohlc_old = pd.read_pickle(path)
+    except FileNotFoundError:
+        print(f"{path} does not exist. Creating...")
+        pass
+    else:
+        print(f"{path} already exists. Loaded...")
+        ohlc = pd.merge_ordered(ohlc, ohlc_old, how="outer")
+    ohlc.to_pickle(path)
+    print(ohlc)
