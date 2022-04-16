c5fa611c9a5ed2acf5541d108066df7efb01fb05
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-04-16 17:12:25 +0200

    UI: Ptyhon curses

---
+++ src/ui_curses.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import curses
+from curses.textpad import Textbox, rectangle
+
+def main(stdscr):
+    stdscr.addstr(0, 0, "Enter IM message: (hit Ctrl-G to send)")
+
+    editwin = curses.newwin(5,30, 2,1)
+    rectangle(stdscr, 1,0, 1+5+1, 1+30+1)
+    stdscr.refresh()
+
+    box = Textbox(editwin)
+
+    # Let the user edit until Ctrl-G is struck.
+    box.edit()
+
+    # Get resulting contents
+    message = box.gather()
+
+stdscr = curses.initscr()
+main(stdscr)
