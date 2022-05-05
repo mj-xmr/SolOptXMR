58eb046c6fba85bb7cc4f28af7b99287aac637cc
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-05-05 20:43:13 +0200

    params change

--- util/ci-debian.sh
+++ util/ci-debian.sh
@@ -21,5 +21,5 @@ make
 export R_HOME=/usr/lib/R && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$R_HOME/lib
 
 src/opti/opti --help
-src/opti/opti --horizon-days 2 --start-day 42
+src/opti/opti --horizon-days 3 --start-day 30
 src/opti/opti --help
