3b62325791405452d9953dcb1baaf93fe42380d0
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-05-05 16:31:11 +0200

    Restored ci tests

--- util/ci-debian.sh
+++ util/ci-debian.sh
@@ -9,10 +9,14 @@ GEN="CodeBlocks - Unix Makefiles"
 #./ci-default
 #cd ../..
 
-#python3 src/tests.py
+python3 src/tests.py
 # Now test unpickling:
-#python3 src/tests.py
+python3 src/tests.py
 
 mkdir -p build && cd build
 cmake .. -G "${GEN}" -D BUILD_QT=OFF
 make
+
+src/opti/opti --help
+src/opti/opti --horizon-days 2 --start-day 42
+src/opti/opti --help
