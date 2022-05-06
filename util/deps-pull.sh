0498841f9c76c8f98e95e6bd4595846080a2d387
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-05-06 14:21:50 +0200

    boost

--- util/deps-pull.sh
+++ util/deps-pull.sh
@@ -10,5 +10,6 @@ cd ../..
 sudo apt install gfortran libffi-dev
 sudo apt install libboost-all-dev # Only JSON is needed for now
 #sudo apt install libffi-dev
+#sudo apt install rapidjson-dev
 
 pip3 install -r requirements.txt
