9f4a391e6842a5c17b56b67f5a93eaba555e883c
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-05-06 13:30:48 +0200

    Update deps-pull.sh

--- util/deps-pull.sh
+++ util/deps-pull.sh
@@ -1,12 +1,14 @@
 #!/bin/sh -e
 
-sudo apt install gfortran libffi-dev
-sudo apt install libboost-all-dev # Only JSON is needed for now
-#sudo apt install libffi-dev
 
-pip3 install -r requirements.txt
 
 
 cd externals/tsqsim/
 ./util/deps-pull.sh
 cd ../..
+
+sudo apt install gfortran libffi-dev
+sudo apt install libboost-all-dev # Only JSON is needed for now
+#sudo apt install libffi-dev
+
+pip3 install -r requirements.txt
