f6f055287cee82c4f385c643bfd2d6792618d541
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-05-24 16:09:19 +0200

    Linux wrap

--- util/deps-pull.sh
+++ util/deps-pull.sh
@@ -4,9 +4,11 @@ cd externals/tsqsim/
 ./util/deps-pull.sh
 cd ../..
 
-sudo apt install gfortran libffi-dev
-#sudo apt install libboost-all-dev # Only JSON is needed for now
-#sudo apt install libffi-dev
-sudo apt install rapidjson-dev
+elif [ "$(uname)" == "Linux" ]; then
+	sudo apt install gfortran libffi-dev
+	#sudo apt install libboost-all-dev # Only JSON is needed for now
+	#sudo apt install libffi-dev
+	sudo apt install rapidjson-dev
+fi
 
 pip3 install -r requirements.txt
