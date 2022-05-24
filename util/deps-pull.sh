fa044c3abeeab771841f450b96c481f0d0c50005
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-05-24 16:12:48 +0200

    Fix

--- util/deps-pull.sh
+++ util/deps-pull.sh
@@ -4,6 +4,8 @@ cd externals/tsqsim/
 ./util/deps-pull.sh
 cd ../..
 
+if [ "$(uname)" == "Darwin" ]; then
+	HOMEBREW_NO_AUTO_UPDATE=1 brew install rapidjson libffi
 elif [ "$(uname)" == "Linux" ]; then
 	sudo apt install gfortran libffi-dev
 	#sudo apt install libboost-all-dev # Only JSON is needed for now
