675d239297411a94dc45497accf43d85e35b4048
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-05-28 19:23:57 +0200

    Tuning: Don't mine on the last day to spare energy

--- src/opti-lib/src/OptiEnProfitSubject.cpp
+++ src/opti-lib/src/OptiEnProfitSubject.cpp
@@ -147,6 +147,7 @@ double OptiSubjectEnProfit::GetVerbose(const EnjoLib::Matrix & dataMat, bool ver
         if (i > n-24)
         {
             // last day - don't mine
+            // Since the algo considers the last day in the horizon as "the end of world", if typically decides to drain the battery to the minimum at the horizon.
             penalitySum += resLocal.sumHashes;
         }
         //penalityUnder.Add(pentalityUndervolted);
