b6e51a3c572ca55295fc536939f9cb93f6cd6d34
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-06-05 12:54:54 +0200

    pip fallback (#54)

--- util/deps-pull.sh
+++ util/deps-pull.sh
@@ -13,4 +13,18 @@ elif [ "$(uname)" == "Linux" ]; then
 	sudo apt install rapidjson-dev
 fi
 
-pip3 install -r requirements.txt
+if pip3 install -r requirements.txt ; then
+	echo "pip succeeded."
+else
+	echo "pip failed. Trying a fallback."
+
+	pip3 install pandas>=1.4.2 pvlib pykrakenapi pyparsing pyrsistent python-dateutil \
+	python-json-config pytz requests scipy six urllib3 \
+	beautifulsoup4 wget cairosvg Pillow geocoder \
+	#pip3 install --upgrade 
+	# Curses menu:
+	#windows-curses==2.3.0 ; sys_platform == 'win32'
+	# Ascii menu:
+	#asciimatics==1.13.0
+fi
+
