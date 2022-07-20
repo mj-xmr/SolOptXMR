852d81936729c812b7da6bb26ced1d6299561a9f
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-07-20 12:57:56 +0200

    CI: segmentation of building and testing (#76)

--- util/ci.sh
+++ util/ci.sh
@@ -19,6 +19,10 @@ python3 src/tests.py
 #util/build-debian.sh
 ./ci-default -o "VERBOSE_FUNCTIONS=OFF"
 
+if [ $# -eq 1 ]; then
+	echo "Production test not requested. Exiting."
+	exit 0
+fi
 echo "Testing the entire production chain:"
 # TODO: Make optional & derive a build script from tsqsim/util/build.py:
 export R_HOME=/usr/lib/R && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$R_HOME/lib
