a645bfe95a1fbea7d1032e545d1bb2120d8decf7
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-07-30 20:49:47 +0200

    Ascii plots now use bars

--- tests/opti-test/src/SolUtilTest.cpp
+++ tests/opti-test/src/SolUtilTest.cpp
@@ -5,21 +5,4 @@
 
 using namespace EnjoLib;
 
-TEST(SolUtil_values_to_ascii1)
-{
-    const double minimum = 0;
-    const double maximum = 10;
-    VecD inp;
-    for (int i = 1; i >= -2; --i)
-    {
-        inp.Add(i);
-    }
-    for (int i = 0; i <= 12; ++i)
-    {
-        inp.Add(i);
-    }
-    SolUtil().GetPercentToAscii(inp, minimum, maximum);
-    LOGL << "values_to_ascii1:" << Nl << SolUtil().GetPercentToAscii(inp, minimum, maximum) << Nl;
-}
-
 
