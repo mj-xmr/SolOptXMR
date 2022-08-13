7ec3dfae3aaa3f1fb08c8222e3ce8dfb6e278665
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-08-13 12:54:49 +0200

    Mac: restored unit test and ignore the production tests failure

--- util/ci.sh
+++ util/ci.sh
@@ -32,11 +32,10 @@ echo "Testing the entire production chain:"
 export R_HOME=/usr/lib/R && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$R_HOME/lib
 if [ "$(uname)" == "Linux" ]; then
 	python3 src/prod.py
-fi
-#else
+else
 	# TODO: Mac OSX suffers from an endless loop (?) in prod.py
 	#python3 src/prod.py
-	#cd build/default-static-release/bin/
-	#./opti --out "/tmp" # todo: should be defaulted in the CI App's CLI
-#fi
+	cd build/default-static-release/bin/
+	./opti # --out "/tmp" # todo: should be defaulted in the CI App's CLI
+fi
 
