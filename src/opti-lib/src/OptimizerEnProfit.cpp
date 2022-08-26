463496c4a5b058d30144220243499845fdef7988
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-08-26 08:07:22 +0200

    Fixed number of iterations

--- src/opti-lib/src/OptimizerEnProfit.cpp
+++ src/opti-lib/src/OptimizerEnProfit.cpp
@@ -93,7 +93,7 @@ void OptimizerEnProfit::RandomSearch()
 
     // A heuristic to get the number of possible combinations.
     // TODO: Should check the variance changes
-    const BigInt maxIter = gmat.Pow(gmat.PowInt(horizonHours, 2), gmat.Sqrt(numComputers));
+    const BigInt maxIter = gmat.Pow(gmat.PowInt(horizonHours, 3), gmat.Pow(numComputers, 1/4.0));
 
     {LOGL << GetT() << "Random search of " << maxIter << " solutions\n"
     << "Hours = " << horizonHours << ", computers = " << numComputers << Nl;}
