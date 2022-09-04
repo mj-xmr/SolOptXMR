bb763397df26c8d279aa28c5bb8bacecf27af0df
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-09-04 18:12:07 +0200

    Replacing rapidjson

--- util/deps-pull.sh
+++ util/deps-pull.sh
@@ -5,14 +5,14 @@ cd externals/tsqsim/
 cd ../..
 
 if [ "$(uname)" == "Darwin" ]; then
-	HOMEBREW_NO_AUTO_UPDATE=1 brew install rapidjson libffi osx-cpu-temp
+	HOMEBREW_NO_AUTO_UPDATE=1 brew install jsoncpp libffi osx-cpu-temp
 	echo "Please enable the `at` scheduler manually:"
 	echo "https://unix.stackexchange.com/questions/478823/making-at-work-on-macos/478840#478840"
 elif [ "$(uname)" == "Linux" ]; then
 	sudo apt install gfortran libffi-dev python3-testresources
 	#sudo apt install libboost-all-dev # Only JSON is needed for now
 	#sudo apt install libffi-dev
-	sudo apt install rapidjson-dev lm-sensors fswebcam at
+	sudo apt install libjsoncpp-dev lm-sensors fswebcam at
 fi
 
 if pip3 install -r requirements.txt ; then
