1bab6934c20828a72361d4a451fa2d9d136c4b96
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-11-23 22:27:46 +0100

    Using hashes of 3rd party software, rather than tags (#146)

--- util/build-p2pool.sh
+++ util/build-p2pool.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -e
 
-VER=2.5
+#VER=2.5
 # https://github.com/SChernykh/p2pool/releases
 REPO=p2pool
 DIR=build
+HASH_LATEST_TAG=6cf1b13500795260fd9b98de393ecd19f5aabd5d # 2.5
 
 mkdir -p $DIR && cd $DIR
 if [ ! -d $REPO ]; then
 	git clone --recursive https://github.com/SChernykh/$REPO.git
 fi
 cd $REPO
-git checkout v${VER}
+#git checkout v${VER}
+git checkout $HASH_LATEST_TAG
 mkdir -p $DIR && cd $DIR
 cmake ..
 make -j`nproc`
